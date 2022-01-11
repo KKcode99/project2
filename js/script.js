d0d6fd6df76cc4162a384b1ce0630f4f542762a6
Karen <example@gmail.com>
2022-01-11 19:51:53 +0400

    Add test js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
 
-};
+// };
 
-const lastFilm = prompt("Назовите один из последних просмотренных вами фильмов?"),
-      markFilm = prompt("На сколько вы его оцените?"),
-      lastFilm2 = prompt("Назовите один из последних просмотренных вами фильмов?"),
-      markFilm2 = prompt("На сколько вы его оцените?");
+// const lastFilm = prompt("Назовите один из последних просмотренных вами фильмов?"),
+//       markFilm = prompt("На сколько вы его оцените?"),
+//       lastFilm2 = prompt("Назовите один из последних просмотренных вами фильмов?"),
+//       markFilm2 = prompt("На сколько вы его оцените?");
 
-personalMovieDB.movies[lastFilm] = markFilm;
-personalMovieDB.movies[lastFilm2] = markFilm2;
-console.log(personalMovieDB);
+// personalMovieDB.movies[lastFilm] = markFilm;
+// personalMovieDB.movies[lastFilm2] = markFilm2;
 
-
-
+// console.log(personalMovieDB);
---
+++ js/testjs.js
@@ -0,0 +1,24 @@
+"use strict";
+
+if (4 == 9){
+    console.log("Ok!");
+} else{
+    console.log("error");
+}
+
+let num = 50;
+// (num == 50) ? console.log("Ok") : console.log("error");
+
+switch(num){
+    case 49:
+        console.log("Not");
+        break;
+    case 50:
+        console.log("Yes");
+        break;
+    case 100:
+        console.log("Not no vtoroe");
+        break;
+    default:
+        console.log("realy nigga?");
+}
