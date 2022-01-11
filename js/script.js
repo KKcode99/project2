b87a83c88b79b93f5047284b51b9dddd917ca959
Karen <example@gmail.com>
2022-01-11 19:01:58 +0400

    Answer the films

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-alert("Welcome, user");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const lastFilm = prompt("Назовите один из последних просмотренных вами фильмов?"),
+      markFilm = prompt("На сколько вы его оцените?"),
+      lastFilm2 = prompt("Назовите один из последних просмотренных вами фильмов?"),
+      markFilm2 = prompt("На сколько вы его оцените?");
+
+personalMovieDB.movies[lastFilm] = markFilm;
+personalMovieDB.movies[lastFilm2] = markFilm2;
+console.log(personalMovieDB);
+
+
+
